8af0c7560765f2bc58b1ff6351035e94da502ec0
rojhat <rojhatbirel43821@gmail.com>
2025-01-26 21:00:37 +0300

    TextArea bileşeni eklendi; stil dosyası oluşturuldu ve genel stil ayarları güncellendi. Hero bileşeninin yüksekliği ve içerik düzeni iyileştirildi. Navbar menüsünün arka plan rengi değiştirildi ve genel arka plan rengi güncellendi.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import TextArea from "./Components/Textarea/TextArea";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <TextArea />
     </>
   );
 }
---
+++ src/Components/Textarea/TextArea.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./TextArea.css";
+
+const TextArea = () => {
+  return (
+    <div className="text-area">
+      <div>
+        Dünya çapında seçkin kahve bölgelerinden özel olarak toplanan
+        çekirdeklerimizi, en taze haliyle tutkuyla kavurarak size benzersiz bir
+        lezzet deneyimi sunuyoruz.
+      </div>
+      <hr></hr>
+      <div>
+        Dünya çapında seçkin kahve tedarikçilerinden özenle seçilmiş, yüksek
+        kaliteli çekirdekler ile benzersiz lezzetler sunuyoruz.
+      </div>
+      <hr></hr>
+      <div>"I have measured out my life with coffee spoons." - T.S. Eliot</div>
+    </div>
+  );
+};
+
+export default TextArea;
