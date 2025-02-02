82d4f9ec6c3be992f2376e4c4479134eb716f557
rojhat <rojhatbirel43821@gmail.com>
2025-02-02 10:32:40 +0300

    Footer bileşenine yeni stil ve içerik eklendi; sosyal medya bağlantısı güncellendi.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,21 +4,23 @@ import Logo2 from "../../assets/Logo2.png";
 import Instagram from "../../assets/instagram.svg";
 const Footer = () => {
   return (
-    <div className="footer-content">
-      <img src={Logo2} alt="logo" className="logo2" />
-      <div className="footer-cards">
-        <h2>Destek & Politikalar</h2>
-        <a href="#">İade ve Geri Ödeme Politikası</a>
-        <a href="#">Hakkımızda</a>
-        <a href="#">Bize Ulaşın</a>
+    <div>
+      <div className="footer-content">
+        <img src={Logo2} alt="logo" className="logo2" />
+        <div className="footer-cards">
+          <h2>Destek & Politikalar</h2>
+          <a href="#">İade ve Geri Ödeme Politikası</a>
+          <a href="#">Hakkımızda</a>
+          <a href="#">Bize Ulaşın</a>
+        </div>
+        <div className="footer-cards">
+          <h2>Bizi Takip Edin</h2>
+          <a href="https://www.instagram.com/birel.r/">
+            <img src={Instagram} alt="logo" className="instagram" />
+          </a>
+        </div>
       </div>
-      <div className="footer-cards">
-        <h2>Bizi Takip Edin</h2>
-        <a href="https://www.instagram.com/birel.r/">
-          <img src={Instagram} alt="logo" className="instagram" />
-        </a>
-      </div>
-      <div className="footer-cards">
+      <div className="footer-cards2">
         <h4>© 2025 Rojhat Birel.</h4>
         <h4>
           This Website Designed By{""}
